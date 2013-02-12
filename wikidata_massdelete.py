d15d127275c803f84b0b1bf8ddbbdea57b5f8ee1
Kunal Mehta <legoktm@gmail.com>
2013-02-12 02:51:48 -0600

    verify that the other item exists before deleting

--- wikidata_massdelete.py
+++ wikidata_massdelete.py
@@ -5,6 +5,8 @@ Copyright (C) 2013 Legoktm
 Licensed as CC-Zero. See https://creativecommons.org/publicdomain/zero/1.0 for more details.
 """
 import time
+import re
+link = re.compile('\[\[(.*?)\]\]')
 try:
     import wikipedia as pywikibot
 except ImportError:
@@ -24,6 +26,14 @@ def main():
             continue
         qid = str(template.get(1).value)
         reason = str(template.get(2).value)
+        dupe = link.search(reason)
+        if not dupe:
+            print 'Error: Cannot parse the deletion reason, skipping.'
+            continue
+        other = pywikibot.Page(wikidata, dupe.group(1))
+        if not other.exists():
+            print 'Uhoh, the other copy doesn\'t exist. Won\'t delete.'
+            continue
         print 'Will delete {0} because: {1}'.format(qid, reason)
         page = pywikibot.Page(wikidata, qid)
         if not page.exists():
