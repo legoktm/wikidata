78bae92be99bdbbda4084d82757779aa4c3f2768
Kunal Mehta <legoktm@gmail.com>
2013-04-23 08:57:55 -0500

    more config options, also possibly breaks things.

--- wikidata_create.py
+++ wikidata_create.py
@@ -7,26 +7,31 @@ Licensed as CC-Zero. See https://creativecommons.org/publicdomain/zero/1.0 for m
 """
 import sys
 import json
+import time
 import pywikibot
 from pywikibot.data import api
+from pywikibot import config
 
-enwp = pywikibot.Site('en','wikipedia')
+config.put_throttle = 0
+
+enwp = pywikibot.Site('en', 'wikipedia')
 repo = enwp.data_repository()
+#pywikibot.handleArgs()
 
-def create_item(lang, title, token=None,check=True,labels=False):
-    wiki = lang+'wiki'
-    qid = repo.get_id(wiki, title)
-    if qid != '-1':
+def create_item(site, title, token=None,check=True,labels=True):
+    wiki = site.dbName()
+    lang = site.language()
+    item = pywikibot.ItemPage.fromPage(pywikibot.Page(site, title))
+    if check and item.exists():
         print 'Has already been imported, will not create.'
         return None
     #fetch the lang links
-    params = {'action':'query',
-              'titles':title,
-              'prop':'langlinks',
-              'lllimit':'max'
+    params = {'action': 'query',
+              'titles': title,
+              'prop': 'langlinks',
+              'lllimit': 'max'
     }
-    local_site = pywikibot.Site(lang, 'wikipedia')
-    req = api.Request(site=local_site, **params)
+    req = api.Request(site=site, **params)
     data = req.submit()
     try:
         links = data['query']['pages'].values()[0]['langlinks']
@@ -34,8 +39,10 @@ def create_item(lang, title, token=None,check=True,labels=False):
         links = []
     print 'Fetched {0} local langlinks.'.format(len(links))
     sitelinks = {wiki:{'site':wiki,'title':title}}
-    labels = {lang:{'language':lang, 'value':title}}
+    labels = {lang:{'language': lang, 'value':title}}
     for link in links:
+        if '#' in link['*']:
+            continue
         s=link['lang'].replace('-','_')+'wiki'
         sitelinks[s]={'site':s,'title':link['*']}
         labels[link['lang']]={'language':link['lang'],'value':link['*']}
@@ -58,13 +65,17 @@ def create_item(lang, title, token=None,check=True,labels=False):
     try:
         done = create.submit()
     except pywikibot.data.api.APIError, e:
+        ec = e
         e = unicode(e)
         if 'already exists' in e or e=='':
             #whatthefuck
-            return create_item(lang, title, token=token, check=check, labels=labels) #fuckit
+            return create_item(site, title, token=token, check=check, labels=labels) #fuckit
         elif 'The external client site did' in e:
+            print 'external client site'
             return '-1' #yeah wtf
         elif 'already used by item' in e:
+            #print 'already used by...'
+            print ec.info
             return '-1'
         else:
             try:
@@ -76,7 +87,8 @@ def create_item(lang, title, token=None,check=True,labels=False):
         print 'Successful!'
     else:
         print done
-    return done['entity']['id'] #send back the qid
+#    time.sleep(3)
+    return done['entity']['id']  # send back the qid
 
 def mass_create(lang, titles, token=None):
     r=[]
@@ -84,15 +96,37 @@ def mass_create(lang, titles, token=None):
         r.append(create_item(lang, title, token=token))
     return r
 
+def allpages():
+    params = {}
+    if '-help' in sys.argv:
+        params['namespace'] = 12
+    else:
+        params['namespace'] = 14
+        params['start'] = 'Q'
+    params['filterredir'] = False
+    token = repo.token(pywikibot.Page(repo, 'Main Page'), 'edit')
+    enwp = pywikibot.Site('en','wikipedia')
+    ap = enwp.allpages(content=True,**params)
+    for page in ap:
+        for t in ['soft redirect','softredir', 'soft link']:
+            if t in page.get().lower():
+                print 'Skipping soft redirect'
+                continue
+        print create_item(enwp, page.title(), token, labels=True)
+if '--allpages' in sys.argv:
+    print 'running allpages'
+    allpages()
+    quit()
+
 if __name__ == "__main__":
-    pywikibot.handleArgs()
+    repo.login()
     #d=['Eschatology: Death and Eternal Life', 'The Spirit of the Liturgy','Truth and Tolerance']
     #print mass_create('en',d)
     #create_item('en','Category:American chemical engineers')
     if len(sys.argv) > 1:
         category=pywikibot.Category(enwp, sys.argv[1])
     else:
-        category=pywikibot.Category(enwp, 'Category:National Hockey League')
-    for subcat in category.subcategories(recurse=True):
-        print subcat
-        x=create_item('en',subcat.title(),labels=True)
+        category=pywikibot.Category(enwp, 'Category:Game developer logos')
+    for i in category.articles(recurse=True, namespaces=[6]):
+        print i
+        x=create_item(enwp,i.title(),labels=True)
