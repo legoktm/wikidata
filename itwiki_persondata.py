0bee9c773c941993d8f6c9dc5dbac554e7032172
Kunal Mehta <legoktm@gmail.com>
2013-04-23 08:58:28 -0500

    convert to threaded queues

--- itwiki_persondata.py
+++ itwiki_persondata.py
@@ -21,53 +21,111 @@ FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 IN THE SOFTWARE.
 """
 import mwparserfromhell as mwparser
+import os
+os.environ['PYWIKIBOT2_DIR'] = '/data/project/legobot/.pywikibot'
 import pywikibot
 import oursql
-import os
+import sys
+import time
+sys.path.append(os.path.expanduser('~'))
 import wikidata_create
-
+import threading
+import Queue
 redirects = ['biografia','bio']
 
-itwiki = pywikibot.Site('it','wikipedia')
-repo = pywikibot.Site().data_repository()
+itwiki = pywikibot.Site('it', 'wikipedia')
+if not itwiki.logged_in():
+    itwiki.login()
+repo = itwiki.data_repository()
+if not repo.logged_in():
+    repo.login()
+fetched = Queue.Queue(maxsize=100)
+processed = Queue.Queue()
+parsed = Queue.Queue()
+insert = Queue.Queue()
 
-mapping = {'Sesso':'sex', #either M or F
-       }
+
+class ParseThread(threading.Thread):
+    """
+    Parse the pages using mwparser
+    reads from processed queue
+    """
+    def __init__(self, queue):
+        threading.Thread.__init__(self)
+        self.queue = queue
+
+    def run(self):
+        while True:
+            page, qid = self.queue.get()
+            code = mwparser.parse(page.get())
+            found = False
+            template = ''  # to make pycharm shut up
+            for template in code.filter_templates():
+                if template.name.lower().strip() in redirects:
+                    found = True
+                    break
+            if not found:
+                print 'Could not find template on '+page.title()
+                return
+            data = dictify(template)
+            d = list()
+            for nm in data:
+                if not data[nm] or len(data[nm]) > 250:
+                    continue
+                d.append((None, page.title(), deqid(qid), nm, data[nm]))
+            parsed.put(d)
+            self.queue.task_done()
+
+
+class InsertThread(threading.Thread):
+    """
+    Inserts into the db
+    reads from parsed queue
+    """
+    def __init__(self, queue, db):
+        threading.Thread.__init__(self)
+        self.queue = queue
+        self.db = db
+        self.cursor = self.db.cursor()
+
+    def run(self):
+        while True:
+            d = self.queue.get()
+            store(self.cursor, d)
+            self.queue.task_done()
+
+
+class GetIDThread(threading.Thread):
+    """
+    Gets the id or creates the item
+    reads from fetched queue
+    """
+    def __init__(self, queue):
+        threading.Thread.__init__(self)
+        self.queue = queue
+
+    def run(self):
+        while True:
+            page = self.queue.get()
+            item = pywikibot.ItemPage.fromPage(page)
+            if not item.exists():
+                qid = wikidata_create.create_item(itwiki, page.title(), token=token, check=False, labels=True)
+            else:
+                qid = item.getID()
+            if not qid or qid == '-1':
+                return
+            processed.put((page, qid))
+            self.queue.task_done()
+
+
+
 
 token = repo.token(pywikibot.Page(repo, 'Main Page'), 'edit')
-
-def parse_page(page, db):
-    text= page.get()
-    code = mwparser.parse(text)
-    found = False
-    for template in code.filter_templates():
-        if template.name.lower().strip() in redirects:
-            found = True
-            break
-    if not found:
-        print 'Could not find template on '+page.title()
-        return
-    #ok lets get the wikidata item
-    qid = repo.get_id('itwiki', page.title())
-    if qid == '-1':
-        qid = wikidata_create.create_item('it', page.title(), token=token, check=False, labels=True)
-    if not qid:
-        return
-
-    cursor = db.cursor()
-    data = dictify(template)
-    d = list()
-    for nm in data:
-        if not data[nm] or len(data[nm]) > 250:
-            continue
-        d.append((None, page.title(), deqid(qid), nm, data[nm]))
-        #d.append((None, page.title(), None, nm, data[nm]))
-
-    store(cursor, d)
 
 
 def deqid(qid):
     return int(qid.replace('q',''))
+
 
 def dictify(template):
     data = {}
@@ -78,27 +136,49 @@ def dictify(template):
 
 def store(cursor, stuff):
     print len(stuff)
-    for x in stuff:
-        print x
-    #print (None, title, qid, param, value)
+        #print (None, title, qid, param, value)
     #page VARCHAR(255), qid INT, param VARCHAR(255), value VARCHAR(255)
     cursor.executemany('INSERT INTO `persondata` VALUES (?, ?, ?, ?, ?)',
-                   stuff)
+                       stuff)
     #(None, title, deqid(qid), param, value)
 
+db = oursql.connect(raise_on_warnings=False,
+                    read_default_file=os.path.expanduser("~/.my.cnf"),
+                    )
 
 
-def main():
-    db = oursql.connect(db='legoktm',
-                        host='localhost',
-                        raise_on_warnings=False,
-                        read_default_file=os.path.expanduser("~/.my.cnf"),
-    )
-    template = pywikibot.Page(itwiki, 'Template:Bio')
-    gen = template.getReferences(onlyTemplateInclusion=True, namespaces=[0], content=True)
-    for page in gen:
-        parse_page(page, db)
+for i in range(2):
+    t = ParseThread(processed)
+    t.setDaemon(True)
+    t.start()
 
+for i in range(2):
+    t = InsertThread(parsed, db)
+    t.setDaemon(True)
+    t.start()
 
-if __name__ == "__main__":
-    main()
+for i in range(1):
+    t = GetIDThread(fetched)
+    t.setDaemon(True)
+    t.start()
+
+template = pywikibot.Page(itwiki, 'Template:Bio')
+gen = template.getReferences(onlyTemplateInclusion=True, namespaces=[0], content=True)
+count = 0
+for page in gen:
+    count += 1
+    if count % 100 == 0:
+        print count
+    put = False
+    while not put:
+        try:
+            fetched.put(page)
+            put = True
+        except Queue.Full:
+            time.sleep(30)
+
+#exit in order
+fetched.join()
+processed.join()
+parsed.join()
+insert.join()
