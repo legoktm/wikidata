78f2354b8636891fbd44bee2154a07a34a9c0a21
Kunal Mehta <legoktm@gmail.com>
2013-06-28 16:13:12 -0700

    remove another wrapper

--- en_removal_wrapper.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#$ -l h_rt=168:00:00
-#$ -l virtual_free=50M
-#$ -j y
-#$ -b y
-#$ -wd /home/legoktm/rewrite
-#$ -o /home/legoktm/public_html/en_removal.log
-
-/home/legoktm/rewrite/pwb.py /home/legoktm/wikidata/enwiki_removal.py -pt:0
