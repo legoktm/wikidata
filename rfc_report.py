18b3dca4fbfa553332e75337e029930f0edb090f
Kunal Mehta <legoktm@gmail.com>
2013-09-11 20:08:11 -0700

    report on closed RfCs too

--- rfc_report.py
+++ rfc_report.py
@@ -25,7 +25,6 @@ import pywikibot
 from wmflabs import db as mysql
 
 site = pywikibot.Site('wikidata', 'wikidata')
-cat = pywikibot.Category(site, 'Category:Requests for comment')
 
 db = mysql.connect('wikidatawiki')
 
@@ -46,18 +45,24 @@ def contributors(page, db):
         res = cur.fetchone()
     return res[0]
 
-text = """{| class="wikitable; sortable"
-|-
-! Name !! Contributors"""
 
-for page in cat.articles(namespaces=[4]):
-    if page.title() == 'Wikidata:Requests for comment':
-        continue
-    pywikibot.output(page)
-    contrib = contributors(page, db)
-    text += '\n|-\n| {0} || {1}'.format(linkify(page), contrib)
+def make_report(cat, pgname):
+    cat = pywikibot.Category(site, cat)
+    text = """{| class="wikitable; sortable"
+    |-
+    ! Name !! Contributors"""
 
-text += '\n|}'
+    for page in cat.articles(namespaces=[4]):
+        if page.title() == 'Wikidata:Requests for comment':
+            continue
+        pywikibot.output(page)
+        contrib = contributors(page, db)
+        text += '\n|-\n| {0} || {1}'.format(linkify(page), contrib)
 
-pg = pywikibot.Page(site, 'User:Legoktm/RfC report')
-pg.put(text, 'Bot: Updating RfC report.')
+    text += '\n|}'
+
+    pg = pywikibot.Page(site, pgname)
+    pg.put(text, 'Bot: Updating RfC report.')
+
+make_report('Category:Requests for comment', 'User:Legoktm/RfC report')
+make_report('Category:Closed requests for comment', 'User:Legoktm/Closed RfC report')
