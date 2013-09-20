7f27e15067b66f0386e46de54ad13e81cf48fcdd
Kunal Mehta <legoktm@gmail.com>
2013-09-20 14:06:19 -0700

    create an actual link rather than using a wikidata template

--- new_properties.py
+++ new_properties.py
@@ -15,6 +15,7 @@ import re
 #action=query&list=recentchanges&rctype=new&rcnamespace=120&format=jsonfm&rclimit=max
 
 site = pywikibot.Site('wikidata', 'wikidata')
+repo = site.data_repository()
 page = pywikibot.Page(site, 'Wikidata:Status updates/Next')
 oneweek = pywikibot.Timestamp.today() - datetime.timedelta(days=7)
 params = {'action': 'query',
@@ -31,14 +32,21 @@ l = []
 for pg in data['query']['recentchanges']:
     l.append(pg['title'].split(':P')[1])
 
+def make_link(pid):
+    prop = pywikibot.PropertyPage(repo, pid)
+    prop.get()
+    label = prop.labels['en']
+    return '[[:d:Property:{0}|{1}]]'.format(pid, label)
+
+
 t = ''
 for pid in l:
-    t += '{{p|' + pid + '}}, '
+    t += '{0}, '.format(make_link('P'+pid))
 if t:
     t = t[:-2]
 else:
     t = 'none'
 
-newtext = re.sub('\* Newest properties:.*?\n', '* Newest properties: '+ t + '\n', page.get())
+newtext = re.sub('\* Newest properties:.*?\n', '* Newest properties: ' + t + '\n', page.get())
 pywikibot.showDiff(page.get(), newtext)
 page.put(newtext, 'Bot: Updating list of new properties')
